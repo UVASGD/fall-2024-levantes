b9166b89799b1b2431a882f2b100857c30bb69d7
Vincent Martinez <vincentcruzmartinez@gmail.com>
2024-10-24 18:00:04 -0400

    got arena set up with weapon drops and music

--- arena_collisions.gd
+++ arena_collisions.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 func _ready():
+	$"../demure".volume_db = 0
+	$"../freaky".volume_db = -80
 	add_static_collisions(self)
 
 func add_static_collisions(node):
@@ -15,3 +17,17 @@ func add_static_collisions(node):
 			node.add_child(static_body)
 		elif child is Node3D:
 			add_static_collisions(child)
+
+
+func _on_area_3d_body_entered(body):
+	if body is Player:
+		$"../demure".volume_db = -10
+		$"../freaky".volume_db = -80
+	pass # Replace with function body.
+
+
+func _on_area_3d_body_exited(body):
+	if body is Player:
+		$"../freaky".volume_db = -10
+		$"../demure".volume_db = -80
+	pass # Replace with function body.
