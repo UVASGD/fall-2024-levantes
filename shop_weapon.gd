341eee960aee3a133fa6cd08d4d0b121367dd421
Vincent Martinez <vincentcruzmartinez@gmail.com>
2024-11-03 22:19:10 -0500

    added shop, and made shop versions of burst, sniper, smg

--- shop_weapon.gd
+++ shop_weapon.gd
@@ -5,13 +5,16 @@ class_name Shop_Weapon extends Node3D
 @export var reserve:int
 @export var husk_weapon_path:String
 var can_sell = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$model.rotation_degrees = $model.rotation_degrees + Vector3(0,1,0)
 	pass
 
 func sell():
