7d504232a7a1c103beb7d50f500e316a52f6fdcd
Saaswath Kumar <saaskum12@gmail.com>
2024-11-22 18:10:42 -0500

    cleaned up explosion.tscn and made it so nade launcher nades play an explosion sound

--- Enemies/wallbreaker/explosion.gd
+++ Enemies/wallbreaker/explosion.gd
@@ -5,21 +5,12 @@ signal ya_got_hit
 @export var damage_amount = 35
 @onready var timer = $Timer
 @onready var animation_player = $explosion_animation_player
-@export var explosion_sound: AudioStreamPlayer3D
 var velocity = Vector3.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass # Replace with function body.
 
-func play_explosion_sound():
-	# Dynamically create an AudioStreamPlayer3D
-	var sound_player = AudioStreamPlayer3D.new()
-	sound_player.stream = explosion_sound
-	sound_player.global_transform = global_transform
-	get_tree().root.add_child(sound_player)  # Add it to the scene tree
-	sound_player.play()
-	sound_player.connect("finished", queue_free)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -27,10 +18,7 @@ func _physics_process(delta):
 	pass
 
 func play_explode_animation():
-	animation_player.play("explode")
-	play_explosion_sound()
-	#$AudioStreamPlayer3D.play()
-	
+	animation_player.play("explode")	
 	
 
 func _on_body_entered(body):
--- Grenade.gd
+++ Grenade.gd
@@ -1,5 +1,5 @@
 extends "res://throwable.gd"
-
+@export var sound: AudioStream
 var explosion = preload("res://projectiles/explosion.tscn")
 # Called when the node enters the scene tree for the first time.
 
@@ -19,7 +19,13 @@ func action():
 	get_tree().root.add_child(explosion_instance)
 	explosion_instance.play_explode_animation()
 	hide()
-	
+	var audio_player = AudioStreamPlayer3D.new()
+	audio_player.stream = sound
+	audio_player.volume_db = 10
+	audio_player.global_transform = self.global_transform
+	get_parent().add_child(audio_player)
+	audio_player.play()
+	audio_player.connect("finished", queue_free)
 	$".".queue_free()
 	await explosion_instance.animation_player.animation_finished
 	
